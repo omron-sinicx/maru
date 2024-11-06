abb44e014202cf1b4ac1b851da6e15fcef9a07a7
denkiwakame <denkivvakame@gmail.com>
2024-11-06 18:00:54 +0900

    mod: affiliations

--- src/components/authors.jsx
+++ src/components/authors.jsx
@@ -10,24 +10,9 @@ export default class Authors extends React.Component {
     const columnMaxLen =
       this.props.authors.length > 4 ? 3 : this.props.authors.length;
     const authorClass = `uk-width-1-${columnMaxLen} uk-width-1-${this.props.authors.length}@m`;
-    const affliationClass = `uk-width-1-${this.props.affiliations.length} uk-margin-small-top`;
+    const affliationClass = `uk-width-1-${this.props.affiliations.length} uk-margin-small-top uk-text-bold uk-text-muted`;
     return (
       <div>
-        <div
-          className="uk-text-primary uk-text-center uk-flex-center uk-grid-collapse"
-          data-uk-grid
-        >
-          {this.props.authors.map((author, idx) => {
-            return (
-              <span className={authorClass} key={'author-' + idx}>
-                <a target="_blank" className="uk-link-toggle" href={author.url}>
-                  {author.name}
-                </a>
-                <sup>{author.affiliation.join(',')}</sup>
-              </span>
-            );
-          })}
-        </div>
         <div
           className="uk-text-primary uk-text-center uk-grid-collapse"
           data-uk-grid
@@ -35,8 +20,13 @@ export default class Authors extends React.Component {
           {this.props.affiliations.map((affiliation, idx) => {
             return (
               <span className={affliationClass} key={'affiliation-' + idx}>
-                <sup>{idx + 1}</sup>
-                {affiliation}
+                <a
+                  className="uk-link-heading"
+                  href={affiliation.url}
+                  target="_blank"
+                >
+                  {affiliation.name}
+                </a>
               </span>
             );
           })}
